eace49c8142be07ad93d19da1ed861f9800714b5
Kira <kira23kira@yandex.ru>
2023-09-15 15:11:49 +0300

    Complete

--- src/MonthData.java
+++ src/MonthData.java
@@ -31,7 +31,12 @@ class MonthData {
         int currentSeries = 0;
         int finalSeries = 0;
         for (int i = 0; i < days.length; i++) {
-            finalSeries = currentSeries < days[i];
+            if (days[i] >= goalByStepsPerDay) {
+                currentSeries++;
+                if (currentSeries > finalSeries) finalSeries = currentSeries;
+            } else {
+                currentSeries = 0;
+            }
         }
         return finalSeries;
     }
